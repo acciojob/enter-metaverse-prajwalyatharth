03b87ada0a1cef8f9a3ce5b7b407be0a7ed513d5
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2025-02-22 17:33:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+// Select the button and counter elements
+let enterButton = document.getElementById("enterBtn");
+let statusElement = document.getElementById("status");
+
+// Add a click event listener to the button
+enterButton.addEventListener("click", function () {
+    statusElement.innerHTML = "<h1>Entered Metaverse</h1>";
+
+});
